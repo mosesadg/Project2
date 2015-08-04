c3a3c5682c32da7b36141de14e2bdbd281a314fb
Anand Moses <moses.adg@gmail.com>
2015-08-04 12:01:28 -0400

    CRUD ofr 1st model

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,6 +1,6 @@
 class VideosController < ApplicationController
   def index
-    
+
     @videos = Video.all.order(:likes).reverse
 
     end
@@ -8,5 +8,41 @@ class VideosController < ApplicationController
     def show
     @video = Video.find(params[:id])
   end
+
+    def new
+    @video = Video.new
+    end
+
+
+    def create
+
+    @video = Video.create!(video_params)
+
+      redirect_to "/videos/#{@video.id}"
+  end
+
+  def edit
+    @video = Video.find(params[:id])
+  end
+
+  def update
+    @video = Video.find(params[:id])
+    @video.update(video_params)
+
+    redirect_to "/videos/#{@video.id}"
+  end
+
+  def destroy
+    @video = Video.find(params[:id])
+    @video.destroy
+    redirect_to "/videos"
+  end
+
+
+private
+def video_params
+  params.require(:video).permit(:subject, :author, :date_created, :likes, :link_url)
+end
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -55,6 +55,14 @@ Rails.application.routes.draw do
   #   end
 
   get "videos" => "videos#index"
+  get "videos/new" => "videos#new"
+
+  post "videos" => "videos#create"
+
   get "videos/:id" => "videos#show"
+
+  get "videos/:id/edit" => "videos#edit"
+  put "videos/:id" => "videos#update"
+  delete "videos/:id" => "videos#destroy"
 
 end
