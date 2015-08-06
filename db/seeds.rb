adc5cba7e6aed88dc2d23de62b1b70c8c4632009
Anand Moses <moses.adg@gmail.com>
2015-08-06 13:26:35 -0400

    updated seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,14 +9,14 @@
 Video.destroy_all
 Comment.destroy_all
 
-video1 = Video.create(subject:"Learn Ruby", link_url:"https://www.youtube.com/embed/Dji9ALCgfpM", author:"Anand", date_created:"123", likes:12)
+video1 = Video.create(subject:"Learn Ruby", link_url:"https://www.youtube.com/embed/Dji9ALCgfpM", author:"Anand", date_created:"August 01, 2015", likes:12)
 
-video2 = Video.create(subject:"Learn Python", link_url:"https://www.youtube.com/embed/N4mEzFDjqtA", author:"AnandM", date_created:"333", likes:21)
+video2 = Video.create(subject:"Learn Python", link_url:"https://www.youtube.com/embed/N4mEzFDjqtA", author:"AnandM", date_created:"August 02, 2015", likes:21)
 
-video3 = Video.create(subject:"Learn Javascript", link_url:"https://www.youtube.com/embed/vZBCTc9zHtI", author:"AnandM", date_created:"333", likes:270)
+video3 = Video.create(subject:"Learn Javascript", link_url:"https://www.youtube.com/embed/vZBCTc9zHtI", author:"AnandM", date_created:"August 03, 2015", likes:270)
 
-video4 = Video.create(subject:"Learn C#", link_url:"https://www.youtube.com/embed/pSiIHe2uZ2w", author:"AnandM", date_created:"333", likes:45)
+video4 = Video.create(subject:"Learn C#", link_url:"https://www.youtube.com/embed/pSiIHe2uZ2w", author:"AnandM", date_created:"August 04, 2015", likes:45)
 
-video5 = Video.create(subject:"Learn JQuery", link_url:"https://www.youtube.com/embed/hMxGhHNOkCU", author:"AnandM", date_created:"333", likes:67)
+video5 = Video.create(subject:"Learn JQuery", link_url:"https://www.youtube.com/embed/hMxGhHNOkCU", author:"AnandM", date_created:"August 05, 2015", likes:67)
 
 comment1 = Comment.create(text_body:"Video 1 looks good, yay!", author:"Mr.Utube", date_created:"2015", video:video1 )
