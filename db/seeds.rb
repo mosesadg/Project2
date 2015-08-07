ef26d721533e63942013a535e0ab2f122952e073
Anand Moses <moses.adg@gmail.com>
2015-08-06 21:20:36 -0400

    seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,19 +9,20 @@
 Video.destroy_all
 Comment.destroy_all
 
-video1 = Video.create(subject:"Learn Ruby", link_url:"https://www.youtube.com/embed/Dji9ALCgfpM", author:"Anand", date_created:"August 01, 2015", likes:12)
+video1 = Video.create(subject:"George Lopez", link_url:"https://www.youtube.com/embed/iPAjcdrbW3c", author:"Anand", date_created:"August 01, 2015", likes:12)
 
-video2 = Video.create(subject:"Learn Python", link_url:"https://www.youtube.com/embed/N4mEzFDjqtA", author:"AnandM", date_created:"August 02, 2015", likes:21)
+video2 = Video.create(subject:"Jerry Seinfeld", link_url:"https://www.youtube.com/embed/NS2waCMeokE", author:"AnandM", date_created:"August 02, 2015", likes:21)
 
-video3 = Video.create(subject:"Learn Javascript", link_url:"https://www.youtube.com/embed/vZBCTc9zHtI", author:"AnandM", date_created:"August 03, 2015", likes:270)
+video3 = Video.create(subject:"Russell Peters", link_url:"https://www.youtube.com/embed/L2JcsFObmJc", author:"AnandM", date_created:"August 03, 2015", likes:270)
 
-video4 = Video.create(subject:"Learn C#", link_url:"https://www.youtube.com/embed/pSiIHe2uZ2w", author:"AnandM", date_created:"August 04, 2015", likes:45)
+video4 = Video.create(subject:"Larry the Cable Guy", link_url:"https://www.youtube.com/embed/dc-a13Rfgak", author:"AnandM", date_created:"August 04, 2015", likes:45)
 
-video5 = Video.create(subject:"Learn JQuery", link_url:"https://www.youtube.com/embed/hMxGhHNOkCU", author:"AnandM", date_created:"August 05, 2015", likes:67)
+video5 = Video.create(subject:"Jeff Dunham", link_url:"https://www.youtube.com/embed/M-lTNx1xFqI", author:"AnandM", date_created:"August 05, 2015", likes:67)
 
-video6 = Video.create(subject:"Learn Javascript", link_url:"https://www.youtube.com/embed/vZBCTc9zHtI", author:"AnandM", date_created:"August 03, 2015", likes:270)
+video6 = Video.create(subject:"Aziz Ansari", link_url:"https://www.youtube.com/embed/dNiQyM9hasY", author:"AnandM", date_created:"August 03, 2015", likes:270)
 
-video7 = Video.create(subject:"Learn C#", link_url:"https://www.youtube.com/embed/pSiIHe2uZ2w", author:"AnandM", date_created:"August 04, 2015", likes:45)
+video7 = Video.create(subject:"Chris Tucker", link_url:"https://www.youtube.com/embed/KV0Iv---Z4o", author:"AnandM", date_created:"August 04, 2015", likes:45)
 
-video8 = Video.create(subject:"Learn JQuery", link_url:"https://www.youtube.com/embed/hMxGhHNOkCU", author:"AnandM", date_created:"August 05, 2015", likes:67)
+video8 = Video.create(subject:"Louis ck", link_url:"https://www.youtube.com/embed/2sIhvXV32gQ", author:"AnandM", date_created:"August 05, 2015", likes:67)
+video9 = Video.create(subject:"Jim Gaffigan", link_url:"https://www.youtube.com/embed/vWZ3LLILccY", author:"AnandM", date_created:"August 05, 2015", likes:67)
 comment1 = Comment.create(text_body:"Video 1 looks good, yay!", author:"Mr.Utube", date_created:"2015", video:video1 )
